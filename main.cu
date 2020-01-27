e09222724166c0deafc034f60b43177eb2911bb7
MazeOfEncryption <inbox@encryptedmaze.net>
2020-01-27 19:56:19 +0000

    Added time elapsed and seeds/sec. (#2)
    * Added time elapsed and seeds/sec.
    
    * replaced tabs with spaces
    
    (cherry picked from commit 5232d2102767b7d1211f00cc685e8bba17188060)

--- main.cu
+++ main.cu
@@ -19,6 +19,7 @@
 #include <stdint.h>
 #include <memory.h>
 #include <stdio.h>
+#include <time.h>
 
 
 #define signed_seed_t int64_t
@@ -276,6 +277,7 @@ int main() {
 
 
     ulong count = 0;
+    time_t startTime = time(NULL), currentTime;
     for (ulong offset = 0; offset < TOTAL_WORK_SIZE;) {
 
         for(int gpu_index = 0; gpu_index < GPU_COUNT; gpu_index++) {
@@ -296,7 +298,11 @@ int main() {
             count += *nodes[gpu_index].num_seeds;
         }
 
-        printf("Searched %lld seeds, found %lld matches \n", offset + WORK_UNIT_SIZE, count);
+        time(&currentTime);
+        int timeElapsed = (int)(currentTime - startTime);
+        ulong numSearched = offset + WORK_UNIT_SIZE;
+        double speed = (double)numSearched / (double)timeElapsed / 1000000.0;
+        printf("Searched %lld seeds, found %lld matches . Time elapsed: %ds. Speed: %.2fm seeds/s.\n", numSearched, count, timeElapsed, speed);
     }
 
     fclose(out_file);
